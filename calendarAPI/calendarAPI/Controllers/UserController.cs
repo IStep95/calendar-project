27c6b8c773a06b97938e3d0e5ba6ba50b2cdd5f1
Ivan Stepanić <ivan.stepanic@changecode.hr>
2018-11-08 21:49:26 +0100

    Init Web API .NET Core 2.1 project

--- calendarAPI/calendarAPI/Controllers/UserController.cs
+++ calendarAPI/calendarAPI/Controllers/UserController.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
 namespace calendarAPI.Controllers
 {
-	public class UserController
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
 	{
-		public UserController()
+		// GET api/values
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> Get()
+		{
+			return new string[] { "User" };
+		}
+
+		// GET api/values/5
+		[HttpGet("{id}")]
+		public ActionResult<string> Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/values
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/values/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/values/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
 		{
 		}
 	}
