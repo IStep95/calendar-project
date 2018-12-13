b70055534a0a431796afde4ac320fb57a5970249
Ivan Stepanić <ivan.stepanic@changecode.hr>
2018-12-13 13:39:20 +0100

    Init calendar page

--- calendar-project/src/app/app.module.ts
+++ calendar-project/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 import { HttpClientModule }    from '@angular/common/http';
+
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +13,7 @@ import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -20,7 +26,13 @@ import { CalendarComponent } from './calendar/calendar.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- calendar-project/src/app/calendar/calendar.component.ts
+++ calendar-project/src/app/calendar/calendar.component.ts
@@ -1,28 +1,143 @@
-import { Component, OnInit } from '@angular/core';
+//#region Imports
+import { Component, 
+         OnInit, 
+         ChangeDetectionStrategy,
+         ViewChild,
+         TemplateRef 
+        } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { AuthenticationService } from '../authentication.service';
 import { Users } from '../Model/Users';
+
+import {
+  startOfDay,
+  endOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addHours
+} from 'date-fns';
+
+import { Subject } from 'rxjs';
+
+import {
+  CalendarEvent,
+  CalendarEventAction,
+  CalendarEventTimesChangedEvent,
+  CalendarView
+} from 'angular-calendar';
+//#endregion
+
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  }
+};
+
 @Component({
   selector: 'app-calendar',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-
+  //#region Component members
+  @ViewChild('modalContent')
+  modalContent: TemplateRef<any>;
   title: string = "Calendar";
+  view: CalendarView = CalendarView.Month;
+  CalendarView = CalendarView;
+  viewDate: Date = new Date();
+  modalData: {
+    action: string;
+    event: CalendarEvent;
+  }
+  actions: CalendarEventAction[] = [
+    {
+      label: '<i class="fa fa-fw fa-pencil"></i>',
+      onClick: ({ event }: { event: CalendarEvent }): void => {
+        this.handleEvent('Edited', event);
+      }
+    },
+    {
+      label: '<i class="fa fa-fw fa-times"></i>',
+      onClick: ({ event }: { event: CalendarEvent }): void => {
+        this.events = this.events.filter(iEvent => iEvent !== event);
+        this.handleEvent('Deleted', event);
+      }
+    }
+  ];
+  refresh: Subject<any> = new Subject();
+  events: CalendarEvent[] = [
+    {
+      start: subDays(startOfDay(new Date()), 1),
+      end: addDays(new Date(), 1),
+      title: 'A 3 day event',
+      color: colors.red,
+      actions: this.actions,
+      allDay: true,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true
+      },
+      draggable: true
+    },
+    {
+      start: startOfDay(new Date()),
+      title: 'An event with no end date',
+      color: colors.yellow,
+      actions: this.actions
+    },
+    {
+      start: subDays(endOfMonth(new Date()), 3),
+      end: addDays(endOfMonth(new Date()), 3),
+      title: 'A long event that spans 2 months',
+      color: colors.blue,
+      allDay: true
+    },
+    {
+      start: addHours(startOfDay(new Date()), 2),
+      end: new Date(),
+      title: 'A draggable and resizable event',
+      color: colors.yellow,
+      actions: this.actions,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true
+      },
+      draggable: true
+    }
+  ];
+  activeDayIsOpen: boolean = true;
+  authUser: Users = null;
+
+  //#endregion
+
   constructor(private authenticationService: AuthenticationService,
-              private router: Router) {
+              private router: Router, private modal: NgbModal) {
         if (!this.authenticationService.IsAuthenticated) {
             this.router.navigate(['/login']);
         }
-        var authUser = this.authenticationService.getUser();
-        if (authUser != null) {
+        this.authUser = this.authenticationService.getUser();
+        if (this.authUser != null) {
           this.authenticationService
-              .getUserById(authUser.UserId)
+              .getUserById(this.authUser.UserId)
               .subscribe(
                 (data: Users) => {
-                    if (!(authUser.SessionId === data['sessionId'])){
+                    if (!(this.authUser.SessionId === data['sessionId'])){
                       console.log("Incorrect session id.");
                       this.router.navigate(['/login']);
                     }
@@ -32,7 +147,9 @@ export class CalendarComponent implements OnInit {
                   this.router.navigate(['/login']);
                 },
                 () => {
-                  console.log("Done: " + authUser.SessionId);
+                  console.log("Done: " + this.authUser.SessionId);
+                  // Show callendar
+                  // Fill user events
                 }
               );
         }
@@ -40,4 +157,56 @@ export class CalendarComponent implements OnInit {
   
   ngOnInit() {
   }
+
+  //#region Functions
+  logOut() {
+    this.authUser = null;
+    this.router.navigate(['/login']);
+  }
+
+  dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
+    if (isSameMonth(date, this.viewDate)) {
+      this.viewDate = date;
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+      }
+    }
+  }
+
+  eventTimesChanged({
+    event,
+    newStart,
+    newEnd
+  }: CalendarEventTimesChangedEvent): void {
+    event.start = newStart;
+    event.end = newEnd;
+    this.handleEvent('Dropped or resized', event);
+    this.refresh.next();
+  }
+
+  handleEvent(action: string, event: CalendarEvent): void {
+    this.modalData = { event, action };
+    this.modal.open(this.modalContent, { size: 'lg' });
+  }
+
+  addEvent(): void {
+    this.events.push({
+      title: 'New event',
+      start: startOfDay(new Date()),
+      end: endOfDay(new Date()),
+      color: colors.red,
+      draggable: true,
+      resizable: {
+        beforeStart: true,
+        afterEnd: true
+      }
+    });
+    this.refresh.next();
+  }
+  //#endregion
 }
