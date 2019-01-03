48256d7252dcef8a59b48f41748b82cb9c8d4dcd
Ivan Stepanić <ivan.stepanic@changecode.hr>
2019-01-03 13:22:14 +0100

    Add remote endpoints to frontend project

--- calendar-project/src/app/Constants.ts
+++ calendar-project/src/app/Constants.ts
@@ -1,19 +1,23 @@
 export class Constants {
 
-    public static API_ENDPOINT_USER:string = "https://localhost:5001/api/users";
+    //public static API_ENDPOINT_USER:string = "https://localhost:5001/api/users";
 
-    public static API_ENDPOINT_EVENT:string = "https://localhost:5001/api/events";
+    //public static API_ENDPOINT_EVENT:string = "https://localhost:5001/api/events";
 
-    public static MIN_PASSWORD_LENGTH:number = 8;
+    public static API_ENDPOINT_USER: string = "https://calendarpwaapi.azurewebsites.net/api/users";
+ 
+    public static API_ENDPOINT_EVENT: string = "https://calendarpwaapi.azurewebsites.net/api/events";
 
-    public static LOADER_ICON_IMAGE_PATH:string = "../assets/images/loader.svg";
+    public static MIN_PASSWORD_LENGTH: number = 8;
 
-    public static _1000MSEC:number = 1000;
+    public static LOADER_ICON_IMAGE_PATH: string = "../assets/images/loader.svg";
 
-    public static BAD_REQUEST:number = 400;
+    public static _1000MSEC: number = 1000;
+
+    public static BAD_REQUEST: number = 400;
     
-    public static UNAUTHORIZED:number = 401;
+    public static UNAUTHORIZED: number = 401;
 
-    public static AUTHENTICATED_USER_KEY = "Authenticated user";
+    public static AUTHENTICATED_USER_KEY: string = "Authenticated user";
 
 } 
