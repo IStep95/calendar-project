429df4275e0debce272df3a5597d1a3649804ef7
Ivan Stepanić <ivan.stepanic@changecode.hr>
2019-01-06 19:42:15 +0100

    Init PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { Injectable, Injector, ErrorHandler, NgZone, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { HttpClientModule }    from '@angular/common/http';
+import { ServiceWorkerModule } from  '@angular/service-worker';
+import {environment} from '../environments/environment';
 
-import { CalendarModule, DateAdapter } from './angular-calendar/';
 import { adapterFactory } from './angular-calendar/date-adapters/date-fns';
 import { FlatpickrModule } from 'angularx-flatpickr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+
+import { CalendarModule, DateAdapter } from './angular-calendar/';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { CalendarComponent, SimplePopUpDialog } from './calendar/calendar.component';
-import { CommonModule } from '@angular/common';
-import { MatDialogModule } from '@angular/material';
-import { Router } from '@angular/router';
 import { SevereErrorHandler } from './severe-error-handler'
+
 
 @NgModule({
   declarations: [
@@ -42,7 +44,8 @@ import { SevereErrorHandler } from './severe-error-handler'
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory
-    })
+    }),
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled:  environment.production })
   ],
   providers: [{provide: ErrorHandler, useClass: SevereErrorHandler, useValue: this}],
   bootstrap: [AppComponent]
